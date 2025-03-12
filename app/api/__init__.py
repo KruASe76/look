5869c17523797791692f0cfb65c2d85b48760d37
KruASe76 <kruanse@ya.ru>
2025-03-12 04:48:26 +0300

    fix docs redirect

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,14 +1,13 @@
 __all__ = ["app"]
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from starlette.middleware.cors import CORSMiddleware
 
 from .route import root_router
 from .util import lifespan
 
 app = FastAPI(title="Look", lifespan=lifespan)
-
-app.include_router(root_router)
 
 # noinspection PyTypeChecker
 app.add_middleware(
@@ -18,3 +17,10 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT"],
     allow_headers=["*"],
 )
+
+app.include_router(root_router)
+
+
+@app.get("/", include_in_schema=False, response_model=None)
+async def docs_redirect() -> ...:
+    return RedirectResponse(url="/docs")
