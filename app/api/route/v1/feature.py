a0c4f6df17bb9f6de0455574647df465bd15527c
KruASe76 <kruanse@ya.ru>
2025-07-24 23:38:16 +0300

    even more feature endpoints...

--- app/api/route/v1/feature.py
+++ app/api/route/v1/feature.py
@@ -1,29 +1,68 @@
+import random
+
 from fastapi import APIRouter, status
 
-from app.core.config import BRANDS_USER_ID, TRENDS_USER_ID
+from app.core.config import SpecialUserIds
 from app.model import CollectionSchema
 from app.service import CollectionService
 
+from ..auth import InitDataUser
 from ..dependencies import DatabaseSession
 
 feature_router = APIRouter(prefix="/feature", tags=["feature"])
 
 
 @feature_router.get(
-    "/trends",
+    "/trends/global",
     response_model=list[CollectionSchema],
     status_code=status.HTTP_200_OK,
     summary="Get trends collections",
 )
-async def get_trends(session: DatabaseSession) -> ...:
-    return await CollectionService.get_by_owner_id(session, TRENDS_USER_ID)
+async def get_global_trends(session: DatabaseSession) -> ...:
+    return await CollectionService.get_by_owner_id(
+        session, SpecialUserIds.GLOBAL_TRENDS
+    )
 
 
 @feature_router.get(
-    "/brands",
+    "/brands/global",
     response_model=list[CollectionSchema],
     status_code=status.HTTP_200_OK,
     summary="Get brands collections",
 )
-async def get_brands(session: DatabaseSession) -> ...:
-    return await CollectionService.get_by_owner_id(session, BRANDS_USER_ID)
+async def get_global_brands(session: DatabaseSession) -> ...:
+    return await CollectionService.get_by_owner_id(
+        session, SpecialUserIds.GLOBAL_BRANDS
+    )
+
+
+@feature_router.get(
+    "/trends/personal",
+    response_model=list[CollectionSchema],
+    status_code=status.HTTP_200_OK,
+    summary="Get trends collections",
+)
+async def get_personal_trends(user: InitDataUser, session: DatabaseSession) -> ...:  # noqa: ARG001
+    result = list(
+        await CollectionService.get_by_owner_id(session, SpecialUserIds.PERSONAL_TRENDS)
+    )
+
+    random.shuffle(result)
+
+    return result
+
+
+@feature_router.get(
+    "/brands/personal",
+    response_model=list[CollectionSchema],
+    status_code=status.HTTP_200_OK,
+    summary="Get brands collections",
+)
+async def get_personal_brands(user: InitDataUser, session: DatabaseSession) -> ...:  # noqa: ARG001
+    result = list(
+        await CollectionService.get_by_owner_id(session, SpecialUserIds.PERSONAL_BRANDS)
+    )
+
+    random.shuffle(result)
+
+    return result
